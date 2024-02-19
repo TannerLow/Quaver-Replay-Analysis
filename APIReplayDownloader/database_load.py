4db9e486cb8809929a93c9e09d850aed95434c73
zachhill7714 <55302876+zachhill7714@users.noreply.github.com>
2024-02-19 12:26:04 -0500

    Using database-load.py to load 4k lb

--- APIReplayDownloader/database_load.py
+++ APIReplayDownloader/database_load.py
@@ -3,14 +3,20 @@ from playerdata import get_leaderboard_by_page
 from time import sleep
 
 
-lb_db = database.open_db("db/leaderboard.fs")
+db = database.open_db("db/leaderboard_4k.fs")
 connection = database.get_connection(db)
 
-for i in range(0, 71):
-    lb_page = get_leaderboard_by_page(i)
+num = 0
+for i in range(1, 201):
+    lb_page = get_leaderboard_by_page(i, 1)  # 1 means 4k
     for user in lb_page:
-        print("Adding lb entry to database: ", database.insert_leaderboard_entry(connection, user))
-    sleep(1)
+        print(f"Adding entry #{num} to 4k leaderboard:", database.insert_leaderboard_entry(connection, user))
+        num += 1
+    if num % 500 == 0:  # pack every 10 pages
+        connection, db = database.pack(connection, db)
+    sleep(3)
+
+connection, db = database.pack(connection, db)  # extra time at the end to make sure
 
 connection.close()
-lb_db.close()
+db.close()
