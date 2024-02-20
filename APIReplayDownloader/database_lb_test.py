722ede91b222a38e37aa13f466456ec8d5abac82
zachhill7714 <zachhill7714@gmail.com>
2024-02-20 13:58:28 -0500

    test if 7k leaderboard has top 1-50

--- APIReplayDownloader/database_lb_test.py
+++ APIReplayDownloader/database_lb_test.py
@@ -3,7 +3,13 @@ import database
 db = database.open_db("db/players.fs")
 connection = database.get_connection(db)
 
+adb = database.open_db("db/testdb.fs")
+aconnection = database.get_connection(adb)
+
 player = database.get_player(connection, 175369)
+print(player)
+
+player = database.get_leaderboard_entry(aconnection, 1)
 print(player)
 
 connection.close()
